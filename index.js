4bd053f2f12d23f2406fc58d07c5a9255c2b47f1
Callum <39635013+Zelatrix@users.noreply.github.com>
2019-02-16 17:39:17 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client;
+
+async run(message, )
+{
+	//Checks if the bot is ready. 
+	message.on("ready", function()
+	{
+		
+	});
+}
